97db0e60bf5eeb9357acd2d67852cdf2aa64466e
Trre-Gen <trregen222@gmail.com>
2018-01-24 21:54:16 +0200

    you-bot.py
    you-bot.py

---
+++ you-bot.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import os
+import sys
+import time
+
+
+try:
+	link = sys.argv[1]
+	print "[+] link alindi"
+except:
+	print "[*] usage : python garanti.py <youtube_link>"
+	exit()
+
+
+def olay():
+	br = webdriver.Firefox()
+	br.get("https://uzbekweb.net")
+	br.find_element_by_name("terms").submit()
+	br.get(link)
+	time.sleep(15)
+	br.close()
+
+say = 0
+dene = 0
+while True:
+	try:
+		olay()
+		say += 1
+		print "olumlu :", say
+	except:
+		dene += 1
+		print "Malesef :", dene
+		if dene == "5":
+			break
+
+print say, " kere acildi...."
